91d87c9db91fef3ea57934f8da40e65bea7a0cbd
Hubaka Ghayati Setyakurnia <hubakag@gmail.com>
2021-01-02 11:30:26 +0700

    Add files via upload

---
+++ flyhaven.py
@@ -0,0 +1,36 @@
+#GNU nano 5.4                             fly.py
+from os import system
+
+flyingvecone = ("""
+/^_____=--
+ ----\_\
+""")
+x = int
+y = int
+mx = int
+my = int
+minx = int
+miny = int
+vecnumber = int
+vec = str
+vec1 = 1
+vecnum = str
+#print ("input vehicle number(1/1)")
+vecnumber = 1
+print ("//vecnumber = " + str(vecnumber));
+if vecnumber == 1:
+ vecnum = vec1
+ print("//vecnum = 1")
+if vecnumber == 1:
+ x = 3
+ y = 2
+ minx = 0
+ miny = 1
+ print ("//center of mass")
+ print ("//in x axis = " + str(x))
+ print ("//in y axis = " + str(y))
+ print ("//min x is = " + str(minx))
+ print ("//min y is = " + str(miny))
+if vecnumber == 1:
+ print (flyingvecone)
+# print ("you have chosen vec " + "(" + str(vecnumber) + ")")
